5d1da2edbdd8b2215277c90f5c0a57a1513ad80e
kelvin-lc <lc_kelvin@163.com>
2026-01-31 18:30:23 +0800

    refactor: add CleanupStep for audio file cleanup

--- src/podcast_to_text/steps/__init__.py
+++ src/podcast_to_text/steps/__init__.py
@@ -4,5 +4,13 @@ from .download import DownloadStep
 from .transcribe import TranscribeStep
 from .format import FormatStep, SkipFormatStep
 from .save import SaveStep
+from .cleanup import CleanupStep
 
-__all__ = ["DownloadStep", "TranscribeStep", "FormatStep", "SkipFormatStep", "SaveStep"]
+__all__ = [
+    "DownloadStep",
+    "TranscribeStep",
+    "FormatStep",
+    "SkipFormatStep",
+    "SaveStep",
+    "CleanupStep",
+]
